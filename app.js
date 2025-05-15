d9247c4c25427c32acab03a280718df849a3bfad
soknanprogramming <poch.soknan@gmail.com>
2025-05-16 05:14:21 +0700

    Okay

--- app.js
+++ app.js
@@ -87,6 +87,73 @@ app.post('/books', async (req, res) => {
     }
 });
 
+// delete a book
+app.delete('/books/:id', async (req, res) => {
+    try {
+        const bookId = req.params.id;
+        
+        // Choose the right query based on ID format
+        let query;
+        const numericId = Number(bookId);
+        
+        if (!isNaN(numericId)) {
+            // If it's a valid number, query by numeric ID
+            query = { _id: numericId };
+        } else if (isValidObjectId(bookId)) {
+            // If it's a valid ObjectId, query by ObjectId
+            query = { _id: new ObjectId(bookId) };
+        } else {
+            // Neither a valid number nor ObjectId
+            return res.status(400).send('Invalid book ID format');
+        }
+        
+        const result = await db.collection('test_books').deleteOne(query);
+        
+        if (result.deletedCount === 0) {
+            return res.status(404).send('Book not found');
+        }
+        
+        res.status(204).send();
+    } catch (error) {
+        console.error('Error deleting book:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// patch a book
+app.patch('/books/:id', async (req, res) => {
+    try {
+        const bookId = req.params.id;
+        const updatedData = req.body;
+        
+        // Choose the right query based on ID format
+        let query;
+        const numericId = Number(bookId);
+        
+        if (!isNaN(numericId)) {
+            // If it's a valid number, query by numeric ID
+            query = { _id: numericId };
+        } else if (isValidObjectId(bookId)) {
+            // If it's a valid ObjectId, query by ObjectId
+            query = { _id: new ObjectId(bookId) };
+        } else {
+            // Neither a valid number nor ObjectId
+            return res.status(400).send('Invalid book ID format');
+        }
+        
+        const result = await db.collection('test_books').updateOne(query, { $set: updatedData });
+        
+        if (result.matchedCount === 0) {
+            return res.status(404).send('Book not found');
+        }
+        
+        res.status(204).send('it updated soknan');
+    } catch (error) {
+        console.error('Error updating book:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 
 initializeApp();
